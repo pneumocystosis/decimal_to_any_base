92ada725f5ad45874eff651916bcdf08c10f6f96
pneumocystosis <69307109+pneumocystosis@users.noreply.github.com>
2020-09-25 09:10:42 +0200

    Update convertisseur_any_base.py

--- convertisseur_any_base.py
+++ convertisseur_any_base.py
@@ -1,71 +1,18 @@
 possible = True
 nbaconv = 1
-base = int(input("Which base? (base-35 max): "))
+base = int(input("Which base do you want to convert to? (base-35 max): "))
 if base > 35 or base <= 0:
     print ("Impossible, the base you want to convert to isn't available with this program!")
     possible = False
 if possible:
-    nbaconv = int(input("Which number should converted to base-{}? ".format(base)))
+    nbaconv = int(input("Which number should be converted to base-{}? ".format(base)))
 lst = []
 nbaconvconst = nbaconv
 while nbaconv > 0 and possible == True:
     q = nbaconv // base
     r = nbaconv % base
-    if r >= 10 and r <= 20:
-        if r == 10:
-            prt = "A"
-        elif r == 11:
-            prt = "B"
-        elif r == 12:
-           prt = "C"
-        elif r == 13:
-           prt = "D"
-        elif r == 14:
-           prt = "E"
-        elif r == 15:
-           prt = "F"
-        elif r == 16:
-           prt = "G"
-        elif r == 17:
-            prt = "H"
-        elif r == 18:
-            prt = "I"
-        elif r == 19:
-            prt = "J"
-        elif r == 20:
-            prt = "K"
-    elif r >= 21 and r <= 30:    
-        if r == 21:
-            prt = "L"
-        elif r == 22:
-            prt = "M"
-        elif r == 23:
-            prt = "N"
-        elif r == 24:
-            prt = "O"
-        elif r == 25:
-            prt = "P"
-        elif r == 26:
-            prt = "Q"
-        elif r == 27:
-            prt = "R"
-        elif r == 28:
-            prt = "S"
-        elif r == 29:
-            prt = "T"
-        elif r == 30:
-            prt = "U"
-    elif r > 30:
-        if r == 31:
-            prt = "V"
-        elif r == 32:
-            prt = "W"
-        elif r == 33:
-            prt = "X"
-        elif r == 34:
-            prt = "Y"
-        elif r == 35:
-           prt = "Z"
+    if r >= 10:
+        prt = chr(r+55)
     else:
         prt = r
     lst.append(prt)
